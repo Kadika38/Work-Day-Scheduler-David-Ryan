c7b79c0e6eec55bfe2ab2e6c59bf9e1d4cca1d42
Kadika <dryan10101@gmail.com>
2022-04-21 13:47:23 -0700

    Everything works now as far as I can tell.  Will do final adjustmants, extra commenting, and README later.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//load in earlier time block input values from local storage if they exist
+var inputValues = ["", "", "", "", "", "", "", "", ""];
+if (localStorage.getItem("inputValuesSto")) {
+    inputValues = JSON.parse(localStorage.getItem("inputValuesSto"));
+};
+
 //show current date using moment.js
 var currentDay = $("#currentDay")[0];
 currentDay.textContent = moment().format("dddd, MMMM Do");
@@ -6,39 +12,48 @@ currentDay.textContent = moment().format("dddd, MMMM Do");
 //creating objects to store time block elements and relate a string value representing their hour for use in moment.js comparisons
 var nine = {
     spot: $("#nine")[0],
-    val: "09"
+    btnSpot: $("#nineBtn")[0],
+    num: "09"
 };
 var ten = {
     spot: $("#ten")[0],
-    val: "10"
+    btnSpot: $("#tenBtn")[0],
+    num: "10"
 };
 var eleven = {
     spot: $("#eleven")[0],
-    val: "11"
+    btnSpot: $("#elevenBtn")[0],
+    num: "11"
 };
 var twelve = {
     spot: $("#twelve")[0],
-    val: "12"
+    btnSpot: $("#twelveBtn")[0],
+    num: "12"
 };
 var one = {
     spot: $("#one")[0],
-    val: "13"
+    btnSpot: $("#oneBtn")[0],
+    num: "13"
 };
 var two = {
     spot: $("#two")[0],
-    val: "14"
+    btnSpot: $("#twoBtn")[0],
+    num: "14"
 };
 var three = {
     spot: $("#three")[0],
-    val: "15"
+    btnSpot: $("#threeBtn")[0],
+    num: "15"
 };
 var four = {
     spot: $("#four")[0],
-    val: "16"
+    btnSpot: $("#fourBtn")[0],
+    num: "16"
 };
 var five = {
     spot: $("#five")[0],
-    val: "17"
+    btnSpot: $("#fiveBtn")[0],
+    num: "17"
 };
 
 //put them all in an array for use in a for loop
@@ -49,9 +64,9 @@ var datey = moment().format("YYYY-MM-DD ");
 //rn is the current date and hour
 var rn = moment().format("YYYY-MM-DD HH");
 
-//loop through all the time blocks and set their colors using moment.js time comparisons
+//loop through all the time blocks and set their colors using moment.js time comparisons, also set input values and add event listeners to save buttons
 for (i = 0; i < all.length; i++) {
-    dateyCurrent = datey + all[i].val;
+    dateyCurrent = datey + all[i].num;
     console.log(moment(rn).isSameOrBefore(dateyCurrent));
     if (moment(rn).isBefore(dateyCurrent)) {
         all[i].spot.setAttribute("class", "col-10 future");
@@ -62,4 +77,23 @@ for (i = 0; i < all.length; i++) {
     if (moment(rn).isSame(dateyCurrent)) {
         all[i].spot.setAttribute("class", "col-10 present");
     };
+    //set time block input values to what was saved in local storage
+    all[i].spot.setAttribute("value", inputValues[i]);
 };
+
+nine.btnSpot.addEventListener("click", function() {saveInput(nine.spot, 0)})
+ten.btnSpot.addEventListener("click", function() {saveInput(ten.spot, 1)})
+eleven.btnSpot.addEventListener("click", function() {saveInput(eleven.spot, 2)})
+twelve.btnSpot.addEventListener("click", function() {saveInput(twelve.spot, 3)})
+one.btnSpot.addEventListener("click", function() {saveInput(one.spot, 4)})
+two.btnSpot.addEventListener("click", function() {saveInput(two.spot, 5)})
+three.btnSpot.addEventListener("click", function() {saveInput(three.spot, 6)})
+four.btnSpot.addEventListener("click", function() {saveInput(four.spot, 7)})
+five.btnSpot.addEventListener("click", function() {saveInput(five.spot, 8)})
+
+
+function saveInput(spotty, num) {
+    console.log("running");
+    inputValues[num] = spotty.value;
+    localStorage.setItem("inputValuesSto", JSON.stringify(inputValues));
+}
